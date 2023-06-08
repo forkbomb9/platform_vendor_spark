9c1bd993f9aedc78ce07e7da4248c7912b176a65
Timi Rautamäki <timi.rautamaki@gmail.com>
2023-06-08 10:23:47 +0000

    Hookup TARGET_USES_GRALLOC1
    Change-Id: Ica9042ece0f680f58f9f4068b91eec7b60b6bdef

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -37,7 +37,8 @@ SOONG_CONFIG_sparkGlobalVars += \
     target_trust_usb_control_path \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
-    uses_egl_display_array
+    uses_egl_display_array \
+    uses_gralloc1
 
 SOONG_CONFIG_NAMESPACES += sparkNvidiaVars
 SOONG_CONFIG_sparkNvidiaVars += \
@@ -58,6 +59,7 @@ endif
 SOONG_CONFIG_sparkGlobalVars_gralloc_handle_has_reserved_size := $(TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE)
 SOONG_CONFIG_sparkGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 SOONG_CONFIG_sparkGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
+SOONG_CONFIG_sparkGlobalVars_uses_gralloc1 := $(TARGET_USES_GRALLOC1)
 SOONG_CONFIG_sparkNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_sparkQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
 SOONG_CONFIG_sparkQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
@@ -70,6 +72,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 TARGET_TRUST_USB_CONTROL_PATH ?= /proc/sys/kernel/deny_new_usb
 TARGET_TRUST_USB_CONTROL_ENABLE ?= 1
 TARGET_TRUST_USB_CONTROL_DISABLE ?= 0
+TARGET_USES_GRALLOC1 ?= false
 
 # Soong value variables
 SOONG_CONFIG_sparkGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
